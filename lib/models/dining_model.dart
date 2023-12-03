4281d182b85d9d8faa783703836f6d583b5433a0
hanu607 <jrsky723@gmail.com>
2023-12-04 01:22:14 +0900

    4.1 Category Name 수정

--- lib/models/dining_model.dart
+++ lib/models/dining_model.dart
@@ -10,7 +10,7 @@ class DiningModel {
   DiningModel.fromJSON(Map<String, dynamic> json)
       : id = json['id'],
         addressName = json['address_name'],
-        categoryName = json['category_name'],
+        categoryName = json['category_name'].substring(6), // '음식점 > ' 제거
         placeName = json['place_name'],
         placeUrl = json['place_url'],
         roadAddressName = json['road_address_name'],
